35e5e94cba0ae73573552ad8cac387c9a082fc49
Miklos Fazkeas <mfazekas@szemafor>
2014-03-30 07:46:19 +0200

    Use step-up within a rescue, and give warning when it's not installed but gem is used from git

--- lib/rack/multipart_related/version.rb
+++ lib/rack/multipart_related/version.rb
@@ -4,8 +4,12 @@ module Rack
     version = $1 if ::File.expand_path('../../..', __FILE__) =~ /\/rack-multipart_related-([\w\.\-]+)/
     version = nil unless /^([^-]+)-([0-9]+)-[^-]+$/.match(version)
     if version.nil? && ::File.exists?(::File.expand_path('../../../../.git', __FILE__))
-      require "step-up"
-      version = StepUp::Driver::Git.last_version
+      begin
+        require "step-up"
+        version = StepUp::Driver::Git.last_version
+      rescue LoadError
+        puts "Warning: no step-up gem is intalled, please install it to use rack-multipart_related from git"
+      end
     end
     version = "0.0.0" if version.nil?
     VERSION = version.gsub(/^v?([^\+]+)\+?\d*$/, '\1')
